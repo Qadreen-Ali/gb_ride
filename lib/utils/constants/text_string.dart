252f3ace59b714ba486db1981a7f6b35c1798e9d
Br7eleven <balajhussain1122@gmail.com>
2025-12-24 13:00:50 +0500

    changed minor changes in the text_strings

--- lib/utils/constants/text_string.dart
+++ lib/utils/constants/text_string.dart
@@ -27,10 +27,8 @@ class GBText {
   static const String orContinuewith = "Or Continue with";
   static const String continuewithGoogle = "Continue with Google";
   static const String continuewithApple = "Continue with Apple";
-  static const String privacyPolicy =
-      "By Continuing you agree to our Privacy Policy.";
-  static const String termsofServices =
-      "By Continuing you agree to our Terms of Services and Privacy Policy.";
+  static const String privacyPolicy = "Privacy Policy.";
+  static const String termsofServices = "Terms of Services";
   static const String alreadyhaveAnAccount = "Already have an Account? Sign In";
   static const String signIn = "Sign In";
   static const String resendcode = "Resend Code";
