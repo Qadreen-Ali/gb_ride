72165a6aa3ad9a012f58f894b4df594a443fe0cb
StillBlack <stillblack2014@gmail.com>
2026-01-06 12:08:23 +0500

    pubspec for dependency

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ import 'package:gb_ride/view/auth/otp_verification_screen.dart';
 import 'package:gb_ride/view/splash/splash_screen.dart';
 import 'package:gb_ride/view/auth/form_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gb_ride/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
