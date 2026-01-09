c06499692576a874fa9e51385729fdf2cac883e3
Qadreen_Ali <aliqadreen@gmail.com>
2026-01-09 15:33:43 +0500

    local files

--- lib/view/module/local/home/bottom_sheet/find_driver_bottom_sheet.dart
+++ lib/view/module/local/home/bottom_sheet/find_driver_bottom_sheet.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gb_ride/view/module/local/home/bottom_sheet/ride_flow/ride_flow_bottom_sheet.dart';
+import 'package:gb_ride/view/module/local/home/bottom_sheet/ride_flow/widgets/auto-accept_tile.dart';
+import 'package:gb_ride/view/module/local/home/bottom_sheet/ride_flow/widgets/booking_button.dart';
+import 'package:gb_ride/view/module/local/home/bottom_sheet/ride_flow/widgets/bottom_sheet_title.dart';
+import 'package:gb_ride/view/module/local/home/bottom_sheet/ride_flow/widgets/location_container.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../../../../../common/text_field.dart';
 import '../../../../../utils/constants/app_sizes.dart';
@@ -7,10 +11,6 @@ import '../../../../../utils/constants/color_string.dart';
 import '../../../../../utils/constants/image_string.dart';
 import '../../../../../utils/constants/primary_button.dart';
 import '../../../../../utils/constants/text_string.dart';
-import '../../../student/home/widgets/auto_accept_tile.dart';
-import '../../../student/home/widgets/booking_button.dart';
-import '../../../student/home/widgets/bottom_sheet_title.dart';
-import '../../../student/home/widgets/location_container.dart';
 
 class FindDriverBottomSheet extends StatefulWidget {
   const FindDriverBottomSheet({super.key});
